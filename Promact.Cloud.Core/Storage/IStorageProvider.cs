47d5647e744558d8ee4438d1e604d58c74876283
Chintan Shah <chintanshah@promactinfo.com>
2015-05-29 22:41:36 +0530

    Initial Commit
    Added required references and created basic interface

---
+++ Promact.Cloud.Core/Storage/IStorageProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promact.Cloud.Core.Storage
+{
+    public interface IStorageProvider
+    {
+    }
+}
